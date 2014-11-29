fcdd7a18028018df1690d3cd6fbbee3d43b181e3
root <root@ip-172-31-6-174.ec2.internal>
2014-11-29 19:23:58 +0000

    Assignment 1

---
+++ TweetMap/views.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from django.http import Http404
+from django.views.decorators.csrf import csrf_protect
+from django.conf import settings
+from twitter_service.models import TwitterServiceTwitterfeed2
+import MySQLdb
+from django.template import RequestContext
+
+
+from django.contrib.staticfiles.storage import staticfiles_storage
+
+def home(request):
+    tweets = TwitterServiceTwitterfeed2.objects.all().filter(x__isnull = False,y__isnull = False)[0:40000]
+    context = {'tweets' : tweets}
+    return render_to_response("tweet-map.html", {'tweets' : tweets}, RequestContext(request))
+
+def tweet_sync(request):
+    return render(request,"tweet-sync.html", locals())
